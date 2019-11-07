913308fa25517b270f8b7680f445904e74870d5f
zerozero-username <zerozero.username@gmail.com>
2019-11-07 12:55:06 -0300

    changes

--- simple-scripts/net-fix.sh
+++ simple-scripts/net-fix.sh
@@ -1,24 +1,25 @@
 #! /bin/bash
 
 printf "%s\n\n" "Warning! This is a quick fix for net issues with resolvconf"
-sleep 0.1
 
 printf "%s\n\n" "Mostly for when services (like certain vpn's....) mess up with the resolv.conf file"
-sleep 0.1
 
 printf "%s\n\n" "Use with caution!"
-sleep 0.3
 
 printf "%s\n\n" "Starting quick net-fix"
-sleep 0.1
+
 printf "%s\n\n" "Removing resolv.conf from /etc/resolv.conf"
 
 sudo rm -r /etc/resolv.conf
 
-sleep 0.1
+
 printf "%s\n\n" "Copying resolv.conf from /var/run/NetworkManager/resolv.conf to /etc/resolv.conf"
 sudo ln -s /var/run/NetworkManager/resolv.conf /etc/resolv.conf
 
-sleep 0.1
+
 printf "%s\n\n" "Updating resolvconf service"
 sudo resolvconf -u
+
+printf "%\n\n" "Updating dhclient!"
+sudo dhclient -r
+sudo dhclient
