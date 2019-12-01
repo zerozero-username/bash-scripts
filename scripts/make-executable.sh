65ec02013b432cdd2fa33344bdeedfe20bc596bd
zerozero-username <zerozero.username@gmail.com>
2019-11-30 23:45:53 -0300

    *

--- scripts/make-executable.sh
+++ scripts/make-executable.sh
@@ -1,22 +1,15 @@
 #!/usr/bin/env bash
 
 ## This is just a little test
-file_name = read -r -q ("Insert the name of the file: [can include filepath]")
-file_ext = read -r -q ("What's the type of file? [.py, .sh and etc]")
-answer = read -r -q ("- ")
+file_name=
+file_ext=
+answer=
+
+read -p "Insert the name of the file: [can include filepath] " file_name
+read -p "What is the type of file? [py, sh and etc] " file_ext
 
 chmod +x "$file_name.$file_ext"
 
 echo "Done!"
-echo "Do you want to execute it? [yes/no]"
-
-    if ["$answer" == "yes"]
-        then
-
-        ./"$file_name.$file_ext"
-
-    else
-        echo "Got it! Exiting...."
-        sleep 1s
-        exit
-
+sleep 1s
+exit
